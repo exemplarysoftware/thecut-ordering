ecb7d6a62f8754f41a9b1b20364ab9a555509c2e
Matt Austin <matt.austin@thecut.net.au>
2013-03-14 14:07:49 +0800

    Renamed ``OrderFormMixin`` to ``OrderMixin``.

--- thecut/ordering/forms.py
+++ thecut/ordering/forms.py
@@ -2,10 +2,11 @@
 from __future__ import absolute_import, unicode_literals
 
 
-class OrderFormMixin(object):
+class OrderMixin(object):
+    """Mixin for a form to set initial data for publish_at and site is set."""
 
     def __init__(self, *args, **kwargs):
-        super(OrderFormMixin, self).__init__(*args, **kwargs)
+        super(OrderMixin, self).__init__(*args, **kwargs)
         # Ensure initial order value matches instance (after it's post-init)
         if self.fields.get('order', False):
             self.fields['order'].initial = self.instance.order
