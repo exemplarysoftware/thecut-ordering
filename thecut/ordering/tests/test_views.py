0f2c3003542bc648a92bedee0fbeec70447c742c
Mark Lockett <mark.lockett@thecut.net.au>
2016-08-17 14:22:52 +0800

    Test submitted a reorder request Refs #8146

--- thecut/ordering/tests/test_views.py
+++ thecut/ordering/tests/test_views.py
@@ -17,11 +17,12 @@ from django.core.urlresolvers import reverse
 from test_app.urls import urlpatterns
 from thecut.ordering.views import AdminReorderView
 from django.http import HttpResponseForbidden
+from django.contrib.contenttypes.models import ContentType
 
 
-class TestOrderingViews(TestCase):
+class TestSecurityOrderingViews(TestCase):
     def setUp(self):
-        super(TestOrderingViews, self).setUp()
+        super(TestSecurityOrderingViews, self).setUp()
         # Create an admin user.
         self.admin_user = User.objects.create_user(
             'admin', 'a@a.com', 'adminpass')
@@ -31,6 +32,10 @@ class TestOrderingViews(TestCase):
         # Create a staff user.
         self.staff_user = User.objects.create_user(
             'staff', 'a@a.com', 'staffpass')
+        content_type = ContentType.objects.get_for_model(OrderingTestModel)
+        permission = Permission.objects.get(codename='change_orderingtestmodel',
+                                            content_type=content_type)
+        self.staff_user.user_permissions.add(permission)
         self.staff_user.is_staff = True
         self.staff_user.save()
 
@@ -63,4 +68,47 @@ class TestOrderingViews(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+    def test_view_accepts_staffuser_with_permission(self):
+        request = self.factory.post('')
+
+        request.user = self.staff_user
+
+        site = AdminSite()
+        admin = OrderingTestModelAdmin(OrderingTestModel, site)
+        response = AdminReorderView.as_view()(request, admin=admin)
+
+        self.assertEqual(response.status_code, 200)
+
+
+class TestReorderingOrderingViews(TestCase):
+    def setUp(self):
+        super(TestReorderingOrderingViews, self).setUp()
+        # Create an admin user.
+        self.admin_user = User.objects.create_user(
+            'admin', 'a@a.com', 'adminpass')
+        self.admin_user.is_superuser = True
+        self.admin_user.save()
+
+        self.factory = RequestFactory()
+
+    def test_view_accepts_superuser(self):
+        ordertest1 = OrderingTestModelFactory.build()
+        ordertest1.save()
+        ordertest2 = OrderingTestModelFactory.build()
+        ordertest2.save()
+        request = self.factory.post('', {'pk':[ordertest2.pk,ordertest1.pk]})
+
+        request.user = self.admin_user
+
+        site = AdminSite()
+        admin = OrderingTestModelAdmin(OrderingTestModel, site)
+        response = AdminReorderView.as_view()(request, admin=admin)
+
+        self.assertEqual(response.status_code, 200)
+
+        ordertests = OrderingTestModel.objects.all()
+
+        self.assertEqual(len(ordertests), 2)
+        self.assertEqual(ordertests[0].pk, ordertest2.pk)
+        self.assertEqual(ordertests[1].pk, ordertest1.pk)
 
