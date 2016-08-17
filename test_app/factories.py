739dfb9fdfbb51f7eaa755e15953a49dd7ee2fda
Mark Lockett <mark.lockett@thecut.net.au>
2016-08-17 10:37:49 +0800

    Add model tests Refs #8146

---
+++ test_app/factories.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from .models import OrderingTestModel
+
+try:
+    import factory
+except ImportError as error:
+    message = '{0}. Try running `pip install factory_boy`.'.format(error)
+    raise ImportError(message)
+
+
+class OrderingTestModelFactory(factory.django.DjangoModelFactory):
+
+    class Meta(object):
+        model = OrderingTestModel
