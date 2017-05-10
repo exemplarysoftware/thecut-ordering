02fd55e4e08d15db72dc835b57dd2ef5c21e19b3
Matt Austin <matt.austin@thecut.net.au>
2017-05-10 16:24:08 +0800

    Move signal connection to app ready().

--- thecut/ordering/apps.py
+++ thecut/ordering/apps.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from . import receivers
 from django import apps
+from django.db.models import signals
 
 
 class AppConfig(apps.AppConfig):
@@ -8,3 +10,6 @@ class AppConfig(apps.AppConfig):
     label = 'ordering'
 
     name = 'thecut.ordering'
+
+    def ready(self):
+        signals.post_init.connect(receivers.set_order)
